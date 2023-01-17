06daea3a8e8bd851379da30a179217323b3aecfd
kdnakt <a.kid.1985@gmail.com>
2023-01-17 13:48:22 +0900

    reply with pong

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
+use std::io::prelude::*;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,10 +10,16 @@ fn main() {
     //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
+    for result in listener.incoming() {
+        match result {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let mut buf = [0; 128];
+                let n = stream.read(&mut buf).unwrap();
+                println!("incoming: {:?}", &buf[..n]);
+                let pong = String::from("+PONG\r\n");
+                let result = stream.write(pong.as_bytes());
+                println!("out: {:?}", result);
             }
             Err(e) => {
                 println!("error: {}", e);
