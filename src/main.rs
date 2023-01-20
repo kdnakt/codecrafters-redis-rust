156c86bf38a89106046cefaebdd256586c1ecc9c
kdnakt <a.kid.1985@gmail.com>
2023-01-21 01:14:44 +0900

    Implement the SET & GET commands

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
 use std::thread::spawn;
+use std::collections::HashMap;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,6 +14,8 @@ fn main() {
 
     for result in listener.incoming() {
         spawn(move || {
+            // TODO: share cache between connections
+            let mut cache: HashMap<String, String> = HashMap::new();
             match result {
                 Ok(mut stream) => {
                     println!("accepted new connection");
@@ -29,6 +32,12 @@ fn main() {
                             },
                             "echo" | "ECHO" => {
                                 echo(&mut stream, req[4]);
+                            },
+                            "set" | "SET" => {
+                                set(&mut stream, req[4], req[6], &mut cache);
+                            },
+                            "get" | "GET" => {
+                                get(&mut stream, req[4], &cache);
                             },
                             _ => {
                                 println!("command not implemented");
@@ -55,6 +64,29 @@ fn pong(stream: &mut TcpStream) {
 fn echo(stream: &mut TcpStream, message: &str) {
     let len = message.len();
     let msg = format!("${len}\r\n{message}\r\n");
+    println!("out: {msg}");
+    let result = stream.write(msg.as_bytes());
+    println!("out: {:?}", result);
+}
+
+fn set(stream: &mut TcpStream, key: &str, value: &str, cache: &mut HashMap<String, String>) {
+    cache.insert(key.to_string(), value.to_string());
+    println!("cache={:?}", cache);
+    let msg = format!("$2\r\nOK\r\n");
+    println!("out: {msg}");
+    let result = stream.write(msg.as_bytes());
+    println!("out: {:?}", result);
+}
+
+fn get(stream: &mut TcpStream, key: &str, cache: &HashMap<String, String>) {
+    println!("cache={:?}", cache);
+    let msg = match cache.get(&key.to_string()) {
+        Some(message) => {
+            let len = message.len();
+            format!("${len}\r\n{message}\r\n")
+        },
+        None => "$-1\r\n".to_string(),
+    };
     println!("out: {msg}");
     let result = stream.write(msg.as_bytes());
     println!("out: {:?}", result);
